9fd548f412aa193e1c41239794a290bd02dcb208
Moi <moiplay300@gmail.com>
2024-03-30 16:55:47 +0100

    diseño de arriba, turno jugador y temporizador funcional, falta lado right,..

---
+++ src/components/temporizador.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, useState } from "react"
+import { Text } from "react-native";
+
+export default function Temp (){
+
+    const [temp, setTemp] = useState({min:5, seg:0});
+    const interval = useRef(false);
+
+
+    useEffect(()=>{
+        interval.current = setInterval(()=>{
+            setTemp(e => {return { ...e, seg: e.seg == 0 ? 59 : e.seg-1 }});
+        }, 1000);
+    }, []);
+
+    useEffect(()=>{
+        if (temp.seg == 0 && temp.min > 0) {
+            setTimeout(()=>setTemp(e => {return { ...e, min: e.min-1 }}), 1000);
+        }
+
+        if (temp.seg == 0 && temp.min == 0) {
+            clearInterval(interval.current);
+        }
+    }, [temp]);
+
+    return (
+        <Text>{temp.min < 10 ? "0"+temp.min : temp.min}:{temp.seg < 10 ? "0"+temp.seg : temp.seg}</Text>
+    )
+}
